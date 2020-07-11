a7068898be17f5ad755b862cbc0f1d6e6ae5ef87
Mohammed Abd El-Hameed <mohamed.hameed@3ddx.com>
2020-07-11 13:14:52 +0200

    creating Show

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -531,18 +531,27 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-  artist_id = request.form.get('artist_id',1)
-  venue_id = request.form.get('venue_id',1)
-  start_time = request.form.get('start_time') #add default time
+  try:
+    artist_id = request.form.get('artist_id',1)
+    venue_id = request.form.get('venue_id',1)
+    start_time = request.form.get('start_time') #add default time
 
-  artist = Artist.query.get(id=artist_id).first()
-  venue = Venue.query.get(id=venue_id).first()
+    show = Show(artist_id=artist_id,venue_id=venue_id,start_time=start_time)
   
+    db.session.add(show)
+
+    db.session.commit()
+    flash('Show was successfully listed!')
+  except:
+    db.session.rollback()
+    flash('An error occurred. Show could not be listed.')
+  finally:
+    db.session.close()
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
 
   # on successful db insert, flash success
-  flash('Show was successfully listed!')
+  # flash('Show was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Show could not be listed.')
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
